63e54fdee39e383aa0f955027c055bbf1b23a819
brennomeneses <brennocorps@gmail.com>
2022-09-06 20:42:38 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './src/routes';
+
+const PORT = 3007;
+const app = express();
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server Running on http://localhost:${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+Setup your .env file:
+```env
+DATABASE_URL="postgresql://admin:12345@localhost:5432/pg_cocierge?schema=public"
+```
