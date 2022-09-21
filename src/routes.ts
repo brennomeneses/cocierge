9b58907ee45d25a1a09252ece25c08fea95cecd4
brennomeneses <brennocorps@gmail.com>
2022-09-20 21:20:31 -0300

    routes and gitignore added

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import ApartmentController from './controllers/apartmentController';
 import TesteController from './controllers/testeController';
 import VisitorController from './controllers/visitorController';
 
@@ -6,9 +7,13 @@ const routes = Router();
 
 const testController = new TesteController();
 const visitorController = new VisitorController();
+const apartmentController = new ApartmentController();
 
 routes.get("/", testController.handle);
 routes.post("/visitors", visitorController.store);
 routes.get("/visitors", visitorController.handle);
+
+routes.get("/apartment", apartmentController.handle);
+routes.post("/apartment", apartmentController.store);
 
 export default routes;
